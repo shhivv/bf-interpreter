fb50218cb1abed3b3dc7f6fb42d721881b954192
Shiv <shiv.7.shan@gmail.com>
2024-12-03 20:21:28 +0530

    initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#define TAPE_SIZE 30000
+#include <stdio.h>
+
+char tape[TAPE_SIZE];
+int cptr = 0;
+int mptr = 0;
+void interpret(char* code, int length){
+  
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("expected bf file\n");
+    return 1;
+  }
+
+}
